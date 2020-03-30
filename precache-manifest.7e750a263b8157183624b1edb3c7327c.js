0d029851432f961d68c733288cf98e8a0499eb5c
Roopin <bhadriroopin748@gmail.com>
2020-03-30 07:45:32 +0530

    Updates

---
+++ precache-manifest.7e750a263b8157183624b1edb3c7327c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e14e1d598f5709a8bcde40134a6432",
+    "url": "/Pokegame/index.html"
+  },
+  {
+    "revision": "1fab2571179ff5076639",
+    "url": "/Pokegame/static/css/main.3539c13f.chunk.css"
+  },
+  {
+    "revision": "a41335c963ddefa63c94",
+    "url": "/Pokegame/static/js/2.72a9ac95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokegame/static/js/2.72a9ac95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fab2571179ff5076639",
+    "url": "/Pokegame/static/js/main.db9bc56f.chunk.js"
+  },
+  {
+    "revision": "2136c21f6e008d433e83",
+    "url": "/Pokegame/static/js/runtime-main.58c6dacd.js"
+  }
+]);
